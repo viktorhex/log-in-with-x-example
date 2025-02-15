c4edded265819891b0c5704ebe08b442406820c0
viktorhex <christopherdillinger200@gmail.com>
2025-02-15 15:58:36 -0500

    update readme

--- app/session-provider-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-'use client'; // Mark this as a client component
-
-import { SessionProvider } from 'next-auth/react';
-import { ReactNode } from 'react';
-
-export function SessionProviderWrapper({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
